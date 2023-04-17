19b845e56f2e3f839e8bbbbc5ea9622dcdf8ca41
Mackrine Awino <awinomackrine1@gmail.com>
2023-04-17 12:20:45 +0300

    Add the comments resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_073506) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_091639) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_073506) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["task_id"], name: "index_categories_on_task_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
