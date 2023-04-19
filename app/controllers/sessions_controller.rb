02cf2e4ead8740b0ba2d0923e0542adff17038dd
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-19 11:50:42 +0300

    add create route to handle signup of new users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+
+
+    def create
+        user = User.find_by(name: params[:name])
+    
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: ["Invalid name or password"] }, status: :unauthorized
+        end
+      end
 end
