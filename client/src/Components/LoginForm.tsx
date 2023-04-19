37a82b6e5247117bb7370ca0e878f377f6d7b6a5
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-19 15:52:21 +0300

    add form fields

--- client/src/Components/LoginForm.tsx
+++ client/src/Components/LoginForm.tsx
@@ -14,33 +14,46 @@ const LoginForm: React.FC<Props> = ({ onLogin }) => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
 
-    // function handleSubmit(e) {
-    //     e.preventDefault();
-    //     setIsLoading(true);
-    
-    //     fetch("/login", {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //       },
-    //       body: JSON.stringify({ username, password }),
-    //     }).then((r) => {
-    //       setIsLoading(false);
-    //       if (r.ok) {
-    //         r.json().then((user) => onLogin(user));
-    //       } else {
-    //         r.json().then((err) => setErrors(err.errors));
-    //       }
-    //     });
-    //   }
+
 
 
 
   return (
-    <div>
-      
-    </div>
-  )
+    <form onSubmit={handleSubmit}>
+      <FormField>
+        <Label htmlFor="username">Username</Label>
+        <Input
+          type="text"
+          id="username"
+          autoComplete="off"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </FormField>
+      <FormField>
+        <Label htmlFor="password">Password</Label>
+        <Input
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </FormField>
+
+      <Button variant="fill" color="primary" type="submit">
+        {isLoading ? "Loading..." : "Login"}
+      </Button>
+
+      <FormField>
+        {errors.map((err) => (
+          <Error key={err}>{err}</Error>
+        ))}
+      </FormField>
+    </form>
+  );
+
+
 }
 
 export default LoginForm
