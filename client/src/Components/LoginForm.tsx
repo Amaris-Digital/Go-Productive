dac07c00fea50b9f3759612d235cb8b0ad78402d
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-19 15:53:56 +0300

    add handleSubmit function that fires on submit

--- client/src/Components/LoginForm.tsx
+++ client/src/Components/LoginForm.tsx
@@ -14,7 +14,25 @@ const LoginForm: React.FC<Props> = ({ onLogin }) => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
 
-
+    function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+        setIsLoading(true);
+    
+        fetch("/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        }).then((r) => {
+          setIsLoading(false);
+          if (r.ok) {
+            r.json().then((user) => onLogin(user));
+          } else {
+            r.json().then((err) => setErrors(err.errors));
+          }
+        });
+      }
 
 
 
