887371fca1a91614b11cf927036e85c53dde8e6d
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-19 16:45:12 +0300

    add colours for button

---
+++ client/src/Styling/Button.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const COLORS = {
+    primary: {
+      "--main": "deepskyblue",
+      "--accent": "white",
+    },
+    secondary: {
+      "--main": "white",
+      "--accent": "lightblue",
+    },
+  };
