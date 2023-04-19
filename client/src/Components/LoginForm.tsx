6a82a5adfdf7ca036230780732bb48381f9d4ab0
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-19 15:43:03 +0300

    import onLogin function from parent component

--- client/src/Components/LoginForm.tsx
+++ client/src/Components/LoginForm.tsx
@@ -1,11 +1,38 @@
 import React, {useState} from 'react';
 
-const LoginForm = () => {
+interface Props {
+    onLogin: React.Dispatch<React.SetStateAction<null>>;
+}
+
+
+
+const LoginForm: React.FC<Props> = ({ onLogin }) => {
 
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [errors, setErrors] = useState<Error[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(false);
+
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setIsLoading(true);
+    
+        fetch("/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        }).then((r) => {
+          setIsLoading(false);
+          if (r.ok) {
+            r.json().then((user) => onLogin(user));
+          } else {
+            r.json().then((err) => setErrors(err.errors));
+          }
+        });
+      }
 
 
 
