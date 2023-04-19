44bb6fe7dfb91e0abddb1b94ab147aa0d3359254
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-19 16:49:28 +0300

    add styling for button base

--- client/src/Styling/Button.ts
+++ client/src/Styling/Button.ts
@@ -10,3 +10,15 @@ const COLORS = {
       "--accent": "lightblue",
     },
   };
+
+  const ButtonBase = styled.button`
+  cursor: pointer;
+  font-size: 1rem;
+  border: 1px solid transparent;
+  border-radius: 20px;
+  padding: 8px 16px;
+  margin-left: 40%;
+  margin-right: 40%;
+  margin-bottom: 10%;
+  text-decoration: none;
+`;
