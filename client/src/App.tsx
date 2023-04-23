c86f9d28ef0911f38f3248a38d4e859d1e8fae61
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-23 15:51:24 +0300

    import child components to parent

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+import Header from "./Components/Header";
+import LandingPage from "./Components/LandingPage";
+//import MainContainer from "./Components/MainContainer";
+import {BrowserRouter, Route, Routes } from "react-router-dom";
+import { Todo } from "./Model/Todo";
+import TodoList from "./Components/TodoList";
+import InputField from "./Components/InputField";
+
+const App: React.FC = () => {
+
+
+  const [user, setUser] = useState<null>(null);
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:3000/me").then((response) => {
+      if (response.ok) {
+        response.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:3000/tasks")
+      .then((response) => response.json())
+      .then((todos) => {
+        console.log(todos);
+        setTodos(todos);
+      });
+  }, []);
+
+
+
+  
+
+  const handleLogout = ():void => {
+    setUser(null);
+  }
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: todos.length +1 , name:todo, completed: false, user_id: Date.now(), }]);
+      setTodo("");
+    }
+  };
+
+  if (!user) return <LandingPage onLogin={setUser} />;
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <span className="heading">Taskify</span>
+      <Header user={user} setUser={setUser} onLogout={handleLogout} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+    {/* <BrowserRouter> */}
+      <Routes>
+      <Route
+          path="/tasks"
+          element={<TodoList todos={todos} setTodos={setTodos} />}
+        />
+      </Routes>
+    
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
