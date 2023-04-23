9ee1734e2c46447bbec78789d2ce405ee15f2968
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-23 13:25:57 +0300

    pass on props to the function

--- client/src/Components/InputField.tsx
+++ client/src/Components/InputField.tsx
@@ -10,7 +10,7 @@ interface Props {
 
   import React from 'react'
   
-  const InputField = () => {
+  const InputField:React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
     return (
       <div>
         
