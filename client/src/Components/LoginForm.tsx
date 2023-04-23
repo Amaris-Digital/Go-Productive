876aa282b01096538ab7e4d3bca2617fc66ce977
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-23 15:47:24 +0300

    fix bug: remove style component feature

--- client/src/Components/LoginForm.tsx
+++ client/src/Components/LoginForm.tsx
@@ -1,77 +1,71 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import styled from "styled-components";
+import "./LoginForm/loginform.css"
+
+// import button from '../Styling/Button';
 
 interface Props {
     onLogin: React.Dispatch<React.SetStateAction<null>>;
 }
 
-
-
 const LoginForm: React.FC<Props> = ({ onLogin }) => {
 
-    const [username, setUsername] = useState<string>("");
+    const [name, setName] = useState<string>("");
     const [password, setPassword] = useState<string>("");
-    const [errors, setErrors] = useState<Error[]>([]);
+    //const [errors, setErrors] = useState([]);
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
 
     function handleSubmit(e: React.FormEvent) {
         e.preventDefault();
         setIsLoading(true);
-    
-        fetch("/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ username, password }),
+        fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name, password }),
         }).then((r) => {
-          setIsLoading(false);
-          if (r.ok) {
-            r.json().then((user) => onLogin(user));
-          } else {
-            r.json().then((err) => setErrors(err.errors));
-          }
+            setIsLoading(false);
+            if (r.ok) {
+                r.json().then((user) => onLogin(user));
+            } else {
+                console.log(Error)
+                //r.json().then((err) => setErrors(err.errors));
+            }
         });
-      }
-
-
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <FormField>
-        <Label htmlFor="username">Username</Label>
-        <Input
-          type="text"
-          id="username"
-          autoComplete="off"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </FormField>
-      <FormField>
-        <Label htmlFor="password">Password</Label>
-        <Input
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </FormField>
-
-      <Button variant="fill" color="primary" type="submit">
-        {isLoading ? "Loading..." : "Login"}
-      </Button>
-
-      <FormField>
-        {errors.map((err) => (
-          <Error key={err}>{err}</Error>
-        ))}
-      </FormField>
-    </form>
-  );
-
-
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <form>
+                <label htmlFor="username">Name</label>
+                <input
+                    type="text"
+                    id="username"
+                    autoComplete="off"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </form>
+            <form>
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </form>
+            <button className='login-button' color="primary" type="submit">
+                {isLoading ? "Loading..." : "Login"}
+            </button>
+            {/* <form>
+                {errors.map((err) => (
+                    <error key={err}>{err}</error>
+                ))}
+            </form> */}
+        </form>
+    );
 }
-
 export default LoginForm
