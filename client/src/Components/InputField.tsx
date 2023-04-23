1f5da4f5bf420435f0414289b11e466144071aab
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-23 13:24:53 +0300

    create interface for props received

---
+++ client/src/Components/InputField.tsx
@@ -0,0 +1,22 @@
+
+
+
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+  }
+
+
+  import React from 'react'
+  
+  const InputField = () => {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+  
+  export default InputField
+  
