57593da3023c90b49c3f3e9eaa9489337fa867b0
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-23 13:18:47 +0300

    create interface for props being received

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,2 +1,12 @@
 import Navbar from "./Navbar"
 import "./styles.css"
+
+interface Props {
+    user:{
+      name: string
+      password: string
+    }
+    setUser: React.Dispatch<React.SetStateAction<null>>
+    onLogout: () => void
+  }
+  
