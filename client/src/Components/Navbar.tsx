8bd69782e24639f76e54e023b603437e972eb443
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-23 10:33:31 +0300

    pass on props to the function(Navbar)

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -9,3 +9,16 @@ interface Props {
     setUser: React.Dispatch<React.SetStateAction<null>>;
     onLogout: () => void;
   }
+
+  import React from 'react'
+  
+  const Navbar = ({ setUser, user, onLogout }) => {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+  
+  export default Navbar
+  
