c681725cfd7501aa762f6a583b329b90faa27aaf
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-23 10:43:24 +0300

    import props for Navbar component

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -1,5 +1,4 @@
 import {Link} from "react-router-dom";
-import "./styles.css"
 
 interface Props {
     user: {
@@ -9,36 +8,3 @@ interface Props {
     setUser: React.Dispatch<React.SetStateAction<null>>;
     onLogout: () => void;
   }
-
-  import React from 'react'
-  
-  const Navbar:React.FC<Props> = ({ setUser, user, onLogout }) => {
-
-    function handleLogoutOnClick(): void {
-        fetch("http://localhost:3000/logout", { method: "DELETE" }).then((r) => {
-          if (r.ok) {
-            setUser(null);
-          }
-        });
-      }
-
-    return (
-        <>
-        <div className="header-background">
-          <div>
-            <p className="welcome">Welcome, {user.name}!</p>
-            <button onClick={handleLogoutOnClick} className="logout-button">
-              Logout
-            </button>
-          </div>
-          {/* <Link to="/comments" className="nav-button"> Your Thoughts? </Link> */}
-          <Link to="/tasks" className="nav-button">
-            Home
-          </Link>
-        </div>
-      </>
-    )
-  }
-  
-  export default Navbar
-  
