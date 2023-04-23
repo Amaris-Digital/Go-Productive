c48ff49945585908f7bac2f1b65208b60f10b20e
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-23 13:41:25 +0300

    import props and pass them on to the function

--- client/src/Components/OneTodo.tsx
+++ client/src/Components/OneTodo.tsx
@@ -2,8 +2,13 @@
 import React from 'react'
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
+import { Todo } from "../Model/Todo";
 
-const OneTodo = () => {
+const OneTodo: React.FC<{
+    todo: Todo;
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  }> = () => {
   return (
     <div>
       
