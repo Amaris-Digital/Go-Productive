865702d8df8f8547f9d23a81a3f4cbea290aa773
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-04-23 13:43:31 +0300

    define useState function for edit condition and editTodo

--- client/src/Components/OneTodo.tsx
+++ client/src/Components/OneTodo.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 import { Todo } from "../Model/Todo";
@@ -8,7 +8,12 @@ const OneTodo: React.FC<{
     todo: Todo;
     todos: Todo[];
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-  }> = () => {
+  }> = ({ todo, todos, setTodos }) => {
+
+
+    const [edit, setEdit] = useState<boolean>(false);
+    const [editTodo, setEditTodo] = useState<string>(todo.name);
+
   return (
     <div>
       
